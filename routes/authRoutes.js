dda78cb9ec57b63268ac0d5ba1f33a29ceb66cd9
Mohd Aqib <aqib@logicwind.com>
2020-11-29 23:17:35 +0530

    added logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -17,10 +17,10 @@ module.exports = app => {
     }
   );
 
-//   app.get('/api/logout', (req, res) => {
-//     req.logout();
-//     res.redirect('/');
-//   });
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
